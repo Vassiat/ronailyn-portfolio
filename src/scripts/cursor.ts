d42bb185c343aaafa205ead8f8685a6f1bb3133d
Adrian <atablante04@gmail.com>
2026-06-11 11:13:23 -0400

    feat: cursor complement enhaced ui/ux
    - Change cursor complement for jingle bell
    - Applied angle logic
    - Changes html template so the cursor passes behind tail-svg element

--- src/scripts/cursor.ts
+++ src/scripts/cursor.ts
@@ -1,4 +1,5 @@
 import styles from './cursor.module.css'
+import jingleBell from '../../public/jingle-bell.svg'
 
 const complementCursor = document.createElement("div");
 
@@ -9,18 +10,39 @@ const mouse = {
         x: 0,
         y: 0,
     },
+    currentAngle: 0,
+    targetAngle: 100,
     animate: function() {
         const xDistance = this.x - this.current.x;
         const yDistance = this.y - this.current.y;
 
-        this.current.x += xDistance * 0.1;
+        const speed = Math.hypot(xDistance, yDistance);
+
+        if (speed > 2) { 
+            this.targetAngle = Math.atan2(yDistance, xDistance) * (180 / Math.PI) + 70;
+        } else {
+            this.targetAngle = 0; 
+        }
+
+        let angleDiff = this.targetAngle - (this.currentAngle || 0);
+        angleDiff = ((angleDiff + 180) % 360 + 360) % 360 - 180;
+        
+        const rotationFriction = speed > 2 ? 0.15 : 0.04;
+        this.currentAngle = (this.currentAngle || 0) + ((angleDiff) * rotationFriction);
+
+        this.current.x += xDistance * 0.08;
         this.current.y += yDistance * 0.1;
 
-        complementCursor.style.transform = `translate3d(${this.current.x}px, ${this.current.y}px, 0)`;
-
+        complementCursor.style.transform = 
+            `
+                translate3d(${this.current.x}px, ${this.current.y}px, 0)
+                rotate(${this.currentAngle}deg)
+                `;
+                
         if (Math.abs(xDistance) < 0.05 && Math.abs(yDistance) < 0.05) {
+            
+            this.currentAngle = (this.currentAngle || 0) + ((angleDiff) * rotationFriction);
             this.animationActive = false;
-
             return;
         }
 
@@ -42,9 +64,13 @@ window.addEventListener('mouseover', (e) => {
     complementCursor.classList.add(styles.show);
 })
 
-
 complementCursor.classList.add(styles.complementCursor);
-complementCursor.textContent = "✨";
+
+fetch(jingleBell.src).then(async (response)=>{
+    return await response.text();
+}).then((response)=>{
+    complementCursor.innerHTML += response;
+})
 
 
 document.body.appendChild(complementCursor);
