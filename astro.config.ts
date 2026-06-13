aff2a2b5f2dfc90f2d1312103cf790bb21950b52
Adrian <atablante04@gmail.com>
2026-06-13 19:50:20 -0400

    feat: add base font

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,42 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     base: "/",
     site: "https://ronailyn.pages.dev",
-    output: "static"
+    output: "static",
+    fonts: [{
+        provider: fontProviders.local(),
+        name: "Minecraft",
+        cssVariable: "--font-minecraft",
+        options: {
+            variants: [
+                // Regular
+                {
+                src: ['./src/assets/fonts/minecraftregular-webfont.woff2'],
+                weight: 'normal',
+                style: 'normal'
+                },
+                // Bold
+                {
+                src: ['./src/assets/fonts/minecraftbold-webfont.woff2'],
+                weight: 'bold',
+                style: 'italic'
+                },
+                // Italic
+                {
+                src: ['./src/assets/fonts/minecraftitalic-webfont.woff2'],
+                weight: 'normal',
+                style: 'italic'
+                },
+                // Bold italic
+                {
+                src: ['./src/assets/fonts/minecraftbolditalic-webfont.woff2'],
+                weight: 'bold',
+                style: 'italic'
+                },
+            ]
+        }
+    }]
 });
